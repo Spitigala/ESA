7fbcd6531d2a4656226b52458359421280dc0b62
Johnathan E. Weisner <johnathan_weisner@yahoo.com>
2014-04-11 14:28:15 -0400

    Added menu system for review.

--- view.rb
+++ view.rb
@@ -3,9 +3,21 @@ class Interface
   end
   
   def who_are_you(name)
+    puts "Welcome to NYCSuggester"
   end
 
-  def menu_options
+  def menu_options(menu_type)
+    puts "Your options are:"
+    case menu_type
+    when :start
+      puts "Create a new user with: new <username>"
+      puts "Log in as a user with: login <username>"
+    when :logged_in
+      puts "Add a new place to your list with: add \"<place_name>\""
+      puts "Rate a place you've been to with: rate \"<place_name>\""
+      puts "Get a random suggestion with: suggest"
+      puts "Show all places with: show all"
+    end
   end
 
   def your_list_of_places
