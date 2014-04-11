5b8320cffe34c837336637025a90a8f4be8db788
Johnathan E. Weisner <johnathan_weisner@yahoo.com>
2014-04-11 13:44:51 -0400

    View added

---
+++ view.rb
@@ -0,0 +1,28 @@
+class Interface
+  def initialize
+  end
+
+  def who_are_you(name)
+  end
+
+  def your_list_of_places
+  end
+
+  def add_place(place)
+  end
+
+  def rate_place(place)
+  end
+
+  def random_place
+  end
+
+  def suggested_place
+  end
+
+  def end_app?
+  end
+
+  def delete_place(place)
+  end
+end
