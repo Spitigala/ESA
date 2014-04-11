6d1eec0a4e3c85db3cdce390afb0e5cfcd01a4fd
Johnathan E. Weisner <johnathan_weisner@yahoo.com>
2014-04-11 12:12:43 -0400

    Model skeleton started

---
+++ model.rb
@@ -0,0 +1,61 @@
+class Users
+  def self.add
+  end
+
+  def self.read
+  end
+
+  def self.update
+  end
+
+  def self.delete
+  end
+end
+
+class Places
+  def self.add
+  end
+
+  def self.read
+  end
+
+  def self.update
+  end
+
+  def self.delete
+  end
+end
+
+class Ratings
+  def self.add
+  end
+
+  def self.read
+  end
+
+  def self.update
+  end
+
+  def self.delete
+  end
+end
+
+class VisitLogs
+  def self.add
+  end
+
+  def self.read
+  end
+
+  def self.update
+  end
+
+  def self.delete
+  end
+end
+
+class Compare
+  def initialize
+  end
+
+  def c
